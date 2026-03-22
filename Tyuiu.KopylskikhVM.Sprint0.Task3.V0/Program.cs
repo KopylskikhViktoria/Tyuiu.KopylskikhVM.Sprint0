7caa676841a3f66586f0463eee2f017a12e0ac48
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-22 20:53:14 +0500

    Task3

---
+++ Tyuiu.KopylskikhVM.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.KopylskikhVM.Sprint0.Task3.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}
