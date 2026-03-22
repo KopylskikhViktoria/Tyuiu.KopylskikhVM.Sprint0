275e94f4a004df190a85c37e4ea48b72ace35276
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-22 19:16:53 +0500

    Task2

---
+++ Tyuiu.KopylskikhVM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.KopylskikhVM.Sprint0.Task2.V0.Test;
+
+using Tyuiu.KopylskikhVM.Sprint0.Task2.V0.Lib;
+
+[TestClass]
+public sealed class DataServiceTest
+{
+    [TestMethod]
+    public void CheckGetMessageValid()
+    {
+        var name = "Виктория";
+        var res = DataService.GetMessage(name);
+
+        Assert.AreEqual("Привет, Виктория", res);
+    }
+}
